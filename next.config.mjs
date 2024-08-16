b55d0e871a42a990b30e0dd6897b6ccd0b35527f
Hugo Ng <nguyenquoctuan61089@gmail.com>
2024-08-16 20:31:08 +0700

    fix video not play on ios

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    headers: [
+        {
+            key: 'ngrok-skip-browser-warning',
+            value: 'skip',
+        },
+    ],
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <h1>Welcome to movies app</h1>;
+  return <>
+    <Link href={`/movies`}>Movies</Link>
+  </>;
 }
